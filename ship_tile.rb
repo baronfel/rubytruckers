09edcfd9d4be326ae4a27236326f5e500f9399eb
Peter Dohogne <peter.j.dohogne@gmail.com>
2013-07-06 14:16:05 -0500

    Propagating validation methods to ShipTile class

--- ship_tile.rb
+++ ship_tile.rb
@@ -1,4 +1,6 @@
 class ShipTile
+  attr_reader(:coordinate)
+
   def initialize(coordinate, north, east, south, west, *contents)
     @coordinate = coordinate
     @sides = {:north => north, :east => east, :south => south, :west => west}
@@ -30,11 +32,50 @@ class ShipTile
     @sides[:west] = temp_north
     @contents.each {|c| c.rotate_counter!}
   end
+
+  def get_side(orientation)
+    @sides[orientation]
+  end
+
+  def valid_orientation?
+    @sides.each {|orientation, side| return false if !side.valid_orientation?(orientation)}
+    true
+  end
+
+  def follows_rules?(adjacent_tile)
+    my_side, adjacent_side = get_sides(adjacent_tile)
+    my_side.follows_rules?(adjacent_side)
+  end
+
+  def is_connected?(adjacent_tile)
+    my_side, adjacent_side = get_sides(adjacent_tile)
+    my_side.is_connected?(adjacent_side)
+  end
+
+  def get_sides(adjacent_tile)
+    if @coordinate.x_loc > adjacent_tile.coordinate.x_loc
+      my_orientation = :west
+      adjacent_orientation = :east
+    elsif @coordinate.x_loc < adjacent_tile.coordinate.x_loc
+      my_orientation = :east
+      adjacent_orientation = :west
+    elsif @coordinate.y_loc > adjacent_tile.coordinate.y_loc
+      my_orientation = :north
+      adjacent_orientation = :south
+    else
+      my_orientation = :south
+      adjacent_orientation = :north
+    end
+    my_side = @sides[my_orientation]
+    adjacent_side = adjacent_tile.get_side(adjacent_orientation)
+    [my_side, adjacent_side]
+  end
 end
 
 class Coordinate
   attr_reader(:x_loc)
   attr_reader(:y_loc)
+  
   def initialize(x, y)
     @x_loc = x
     @y_loc = y
