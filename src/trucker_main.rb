ab6ef7b0cb1f9ca21ab79d6d6577f6d73d56aeca
Chester Husk III <chusk3@gmail.com>
2013-08-10 18:42:50 -0500

    more state logic.

--- src/trucker_main.rb
+++ src/trucker_main.rb
@@ -4,17 +4,31 @@ class TruckerMain
 
   attr_accessor :state
 
-  def initialize(num_players)
-    @state = TruckerState.new num_players
+  def initialize(*player_names)
+    @state = TruckerState.new player_names
+  end
+
+  def init_board
+    # code here
+  end
+
+  def processTurn
+    # code here
+  end
+
+  def display_score
+    # code here
   end
 
 end
 
-x = TruckerMain.new(2)
-puts x.num_players
-puts x.game_stage
-puts x.round_number
+x = TruckerMain.new('Peter', 'Chet')
 
+x.init_board
+while x.state != :end
+  x.processTurn
+end
+x.display_score
 
 # Initialize boards for each player, need to do this from the round 1 pattern
   # inflate designs
@@ -26,8 +40,9 @@ puts x.round_number
 
 
 module TruckerGameStage
-  :init
+  :init_truckers
   :construct
   :explore
   :score
+  :end_truckers
 end
--- src/trucker_state.rb
+++ src/trucker_state.rb
@@ -1,3 +1,20 @@
 class TruckerState
-  # To change this template use File | Settings | File Templates.
+  attr_accessor :num_players
+  attr_accessor :game_stage
+  attr_accessor :round_number
+
+  def initialize(player_names)
+      @num_players = player_names.count
+      @game_stage = :init_truckers
+      @round_number = 1
+      @players = Hash[player_names.map{|name| [name, 0] }]
+  end
+
+  def get_score(player_name)
+      @players[player_name]
+  end
+
+  def set_score(player_name, score)
+    @players[player_name] = score
+  end
 end
